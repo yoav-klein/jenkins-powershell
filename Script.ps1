3cb07dabcc284df2c6d0f3e161a7a84bf237c5ad
yoavkl <YOAVKL@rafael.co.il>
2021-01-12 22:29:46 +0200

    Changing script.ps1

--- Script.ps1
+++ Script.ps1
@@ -1,4 +1,25 @@
 
 
-# A cmdlet that throws an exception
+<#
+First Case
+-----------
+
+A cmdlet that throws an exception
+This script will execute and finish in SUCCESS.
+
 Copy-Item invalid_path invalid_dir
+#>
+
+#------------------------------
+
+
+<#
+Second case
+-------------
+A cmdlet that throws an exception
+this time with ErrorAction Stop
+
+
+#>
+
+Copy-Item -ErrorAction Stop invalid_path invalid_dir
