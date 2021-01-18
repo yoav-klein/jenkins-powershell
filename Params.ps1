3b67d1ef694cb1e7f33ecf67e0e25979e59cfafd
yoav-klein <57894892+yoav-klein@users.noreply.github.com>
2021-01-18 13:20:21 +0200

    Update Params.ps1

--- Params.ps1
+++ Params.ps1
@@ -1,6 +1,17 @@
-
+<#
 param([string]$Name,
        [bool]$Upload)
 
 echo $Name
 echo $Upload
+#>
+
+param([string]$Name,
+      [switch]$IsCool
+      )
+      
+if($IsCool) {
+   echo "Yoav is cool"
+} else {
+  echo "Yoav isn't cool"
+}
